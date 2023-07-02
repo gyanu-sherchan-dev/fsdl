a84b94851791b037f874ad229e3452a7df197ae3
Gyanu <gyanusherchan@outlook.com>
2023-07-02 23:22:28 +1000

    react js-cmp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import './App.css';
-import Video from './components/Video';
+import "./App.css";
+import Video from "./components/Video";
 
 function App() {
-  let obj = {
-    title: 'React JS tutorial',
-    views: '999K',
-    time: '1 year ago',
-    channel: 'Coder Dost',
-  };
   return (
-    <div className="App">
-      <div>Videos</div>
-      <Video {...obj}></Video>
-      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
-      <Video
-        title="Mongo DB tutorial"
-        views="1M"
-        time="1 month ago"
-        channel="Coder Dost"
-      ></Video>
+    <div>
+      <div>Hello</div>
+      <Video />
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,19 +1,22 @@
-import './Video.css';
+import "./Video.css";
 
-function Video({title,channel="Coder Dost",views,time}) {
+function Video({ title, channel = "Code Maniac", views, time }) {
   return (
-      <>
-      <div className='container'>
-      <div className="pic">
-      <img src="http://placeimg.com/160/90/1" alt="Katherine Johnson" />
+    <>
+      <div className="container">
+        <div className="pic">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Katherine_Johnson_1983.jpg/1200px-Katherine_Johnson_1983.jpg"
+            alt="Katherine Johnson"
+          />
+        </div>
+        <div className="title">{title}</div>
+        <div className="channel">{channel}</div>
+        <div className="views">
+          {views} views <span>.</span> {time}
+        </div>
       </div>
-      <div className="title">{title}</div>
-      <div className="channel">{channel}</div>
-      <div className="views">
-        {views} views <span>.</span> {time}
-      </div>
-      </div>
-      </>
+    </>
   );
 }
 
