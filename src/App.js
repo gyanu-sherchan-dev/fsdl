8e3765d105fe0b855c29d387ae24f411de24704d
Gyanu <gyanusherchan@outlook.com>
2023-07-03 20:47:35 +1000

    local commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div>
       <div>Hello</div>
-      <Video title="React Js" views="100k" time="1 month" />
-      <Video title="Mongo" views="120k" time="3 month" />
+      <Video />
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -5,7 +5,10 @@ function Video({ title, channel = "Code Maniac", views, time }) {
     <>
       <div className="container">
         <div className="pic">
-          <img src="https://loremflickr.com/640/600" alt="Kitty" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Katherine_Johnson_1983.jpg/1200px-Katherine_Johnson_1983.jpg"
+            alt="Katherine Johnson"
+          />
         </div>
         <div className="title">{title}</div>
         <div className="channel">{channel}</div>
