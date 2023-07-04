fc7302d38dbdc7e54b6aa263a4b0c3d1ad2a459c
Gyanu <gyanusherchan@outlook.com>
2023-07-04 12:57:31 +1000

    props destructuring

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import "./App.css";
 import Video from "./components/Video";
 
 function App() {
+  const obj = {
+    title: "Node.js",
+    views: "200K",
+    time: "4 months",
+  };
+
   return (
     <div>
       <div>Hello</div>
-      <Video />
+      <Video {...obj} />
+      <Video title="Mongo" views="120k" time="3 month" />
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,16 @@
 import "./Video.css";
 
 function Video({ title, channel = "Code Maniac", views, time }) {
+  console.log({ title, channel });
   return (
     <>
       <div className="container">
         <div className="pic">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Katherine_Johnson_1983.jpg/1200px-Katherine_Johnson_1983.jpg"
-            alt="Katherine Johnson"
-          />
+          <img src="https://loremflickr.com/640/600" alt="Kitty" />
         </div>
-        <div className="title">{title}</div>
+        <div className="title" style={{ fontSize: "1rem", color: "black" }}>
+          {title}
+        </div>
         <div className="channel">{channel}</div>
         <div className="views">
           {views} views <span>.</span> {time}
